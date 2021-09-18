284ee5497e73ad6875b2efb233aad9e06418f434
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-09-18 22:25:20 +0700

    chore: add api controllers

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const { mongoDB } = require("./model/index");
+const LikesController = require("./controllers/likes");
+const MatchesController = require("./controllers/matches");
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
