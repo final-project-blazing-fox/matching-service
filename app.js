fc8dd816c19f6bc8622bc730c810d12875f1e99b
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-09-19 16:06:13 +0700

    chore: testing framework preparation

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const { mongoDB } = require("./model/index");
 const LikesController = require("./controllers/likes");
 const MatchesController = require("./controllers/matches");
 
-const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,13 +11,4 @@ app.get("/likes/:id", LikesController.getLikesById);
 app.get("/likes", LikesController.getAllLikes);
 app.post("/likes", LikesController.createLikes);
 
-mongoDB
-  .run()
-  .then(() => {
-    app.listen(3000, () => {
-      console.log(`Running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+module.exports = app;
--- db/config.js
+++ db/config.js
@@ -1,17 +1,18 @@
 const { MongoClient } = require("mongodb");
+const testData = require("./test.json");
 
 const settings = {
   development: {
     uri: process.env.DATABASE_URI_DEVELOPMENT,
     databaseName: process.env.DATABASE_NAME_DEVELOPMENT,
-    collection: process.env.DATABASE_COLLECTION_DEVELOPMENT
+    collection: process.env.DATABASE_COLLECTION_DEVELOPMENT,
   },
   test: {
     uri: process.env.DATABASE_URI_TEST,
     databaseName: process.env.DATABASE_NAME_TEST,
-    collection: process.env.DATABASE_COLLECTION_TEST
-  }
-}
+    collection: process.env.DATABASE_COLLECTION_TEST,
+  },
+};
 
 const db = ({ uri, databaseName }) => {
   const client = new MongoClient(uri);
@@ -33,13 +34,32 @@ const db = ({ uri, databaseName }) => {
     getDB: () => {
       return this.database;
     },
-    closeDb: () => {
+    closeDB: () => {
       client.close();
+    },
+    bulkInsert: () => {
+      const bulk = this.database
+        .collection(settings[process.env.NODE_ENV]["collection"])
+        .initializeUnorderedBulkOp();
+      testData.forEach((data) => {
+        bulk.insert(data);
+      });
+      bulk.execute();
+    },
+    dropCollection: async () => {
+      const collections = (await this.database.listCollections().toArray()).map(
+        (collection) => collection.name
+      );
+      if (collections.includes(settings[process.env.NODE_ENV]["collection"])) {
+        this.database
+          .collection(settings[process.env.NODE_ENV]["collection"])
+          .drop();
+      }
     },
   };
 };
 
 module.exports = {
   db,
-  settings
+  settings,
 };
