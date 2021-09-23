b65cfc9ab27f060221c2c990841a9653b10b85f5
Arip Budiman <aripbudiman007@gmail.com>
2021-09-24 05:54:05 +0700

    feat: add authentication

---
+++ middleware/authentication.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const authentication = async (req, res, next) => {
+  // console.log(req.headers.access_token);
+
+  if (!req.headers.access_token) {
+    return next(new Error({ name: "NOT_LOGIN" }));
+  }
+
+  try {
+    const decoded = await jwt.verify(
+      req.headers.access_token,
+      process.env.JWT_SECRET
+    );
+
+    if (decoded) {
+      req.userId = decoded.id;
+      next();
+    }
+  } catch (err) {
+    next({ name: "INVALID_ACCESS_TOKEN" });
+  }
+};
+
+module.exports = authentication;
